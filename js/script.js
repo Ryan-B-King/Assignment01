fcedd379b11823342ce37bf4e211a748f7207b59
KING-PC\King <ryan13king@gmail.com>
2020-02-06 00:22:54 -0800

    Assignment01 - Completed
    Completed Homework.

---
+++ js/script.js
@@ -0,0 +1,147 @@
+// STEP 01
+
+// var someMonth;
+// function theMonth();
+// var currentMonth;
+// var summerMonth;
+// var myLibraryFunction;
+
+// STEP 02
+
+// 5
+// "String Literal"
+// true
+// null
+
+// STEP 03
+
+// let sampleA = varA + varB + varC;
+// let sampleB = (4 / 6) - 1;
+
+// STEP 04
+
+// let strFirstName, strLastName, strAddress, strCity, strState, numberZipCode, numberYourAge, strReferralSource, boolMayWeContactYou;
+
+// STEP 05
+
+// let strFirstName = window.prompt("What is your first name?");
+// let numberYourAge = 25;
+// const strCity = "San Diego";
+
+// STEP 06
+
+// let test;
+// test = 5 + "12";
+// window.console.log(test);
+
+// STEP 07
+
+// let sampleA = true + " lies";
+// window.console.log(sampleA);
+
+// let sampleB = 10 + false;
+// window.console.log(sampleB);
+
+// STEP 08
+
+// var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// window.console.log(someString);
+
+// STEP 09
+
+// let varNull = null;
+// window.console.log(varNull);
+
+// let varUndefined;
+// window.console.log(varUndefined);
+
+// STEP 10
+
+// let strTest = "String Literal", numTest = 5, boolTest = true, objTest = new function(){}, undefinedTest;
+// window.console.log(typeof strTest, typeof numTest, typeof boolTest, typeof objTest,  typeof undefinedTest);
+
+// STEP 11
+
+// window.alert("Hello " + "Zak " + "Ruvalcaba" + ", " + "welcome " + "to " + "the " + "JavaScript " + "class"+ "!");
+// window.alert("Hello "+ "Ryan King" + ", welcome to the JavaScript class!");
+
+// STEP 12
+
+// let name = "Ryan King";
+// window.alert("Hello "+ name + ", welcome to the JavaScript class!");
+
+// STEP 13
+
+// let name = "Ryan King";
+// let course = "JavaScript";
+// window.alert("Hello "+ name + ", welcome to the " + course + " class!");
+
+// STEP 14
+
+// let name = "Ryan King";
+// let course = "JavaScript";
+// window.alert("Hello "+ name + "." + "\nWelcome to the " + course + " class!");
+
+// STEP 15
+
+// let name = window.prompt("What your full name?");
+// let course = "JavaScript";
+// window.alert("Hello "+ name + "." + "\nWelcome to the " + course + " class!");
+
+// STEP 16
+
+// let name = window.prompt("What your full name?");
+// let course = window.prompt("What course are you taking?");
+// window.alert("Hello "+ name + "." + "\nWelcome to the " + course + " class!");
+
+// STEP 17
+
+// let x = 10;
+// let y = 20;
+// window.console.log(x + y);
+
+// STEP 18
+
+// let x = 20;
+// x += 20;
+// window.console.log(x);
+
+// STEP 19
+
+// let x = 20;
+// x *= 5;
+// window.console.log(x);
+
+// STEP 20
+
+// let x = 20%3;
+// x /= 1;
+// window.console.log(x);
+
+// STEP 21
+
+// let x = 5;
+// let y = 10;
+// window.console.log(x < y && x !== y);
+
+
+// STEP 22
+
+// let x = 15;
+// let y = 1;
+// window.console.log(x < y || x === y);
+
+// STEP 23
+
+// let widget = new Object ();
+// window.console.log(typeof widget);
+
+// STEP 24
+
+// let widget = new Object();
+// window.console.log(widget instanceof Object);
+
+// STEP 25
+
+// let widget = new Object();
+// window.console.log(!widget instanceof Object);
