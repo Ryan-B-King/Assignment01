9ae783a7bc5ce6d7cfeceed9ae660b28f4dfe633
Ryan King <33941531+Ryan-B-King@users.noreply.github.com>
2023-02-15 00:55:54 -0800

    Assignment in progress
    Assignment in progress

---
+++ js/script.js
@@ -0,0 +1,83 @@
+// STEP 01
+
+    //let someMonth;
+    // function theMonth();
+    // let currentMonth;
+    // let summerMonth;
+    // let myLibraryFunction;
+
+// STEP 02
+
+    //Example of numeric literal expression
+    
+    // 100
+    
+    //Example of a string literal expression
+    
+    // 'literal string expression'
+
+    //Example of a Boolean literal expression
+    
+    // true
+
+    //Example of a null literal expression
+
+    // null
+
+// STEP 03
+
+    // Two examples of completes / vaiable expressions
+
+    // let exampleOne = 8 / 2 + (6-1);
+    // let exampleTwo = variableA + variableB + variableC; 
+
+// STEP 04
+
+    // let strFirstName, strLastName, strAddress, strCity, strState, numZipCode, numYourAge, strReferralSource, boolMayWeContactYou;
+
+// STEP 05
+
+    // let strFirstName = window.prompt('What is your first name?');
+    // let numYourAge = 25;
+    // let boolMayWeContactYou = false;
+
+// STEP 06
+
+    // let total;
+    // total = 22 + " years";
+    // console.log(total);
+
+// STEP 07
+
+    // let power;
+    // power = true + ' destruction';
+    // console.log(power);
+
+    // let sleep;
+    // sleep = 'so ' + false;
+    // console.log(sleep);
+
+// STEP 08
+
+    // The example is wrong.  The apostrophe in "I'm" needs to be escaped out with a backslash to indicate it's still part of the string and not the end of the intial quote.
+    // let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+    // onsole.log(someString);
+
+// STEP 09
+
+    let nothing = null;
+    console.log(nothing);
+
+// STEP 10
+// STEP 11
+// STEP 12
+// STEP 13
+// STEP 14
+// STEP 15
+// STEP 16
+// STEP 17
+// STEP 18
+// STEP 19
+// STEP 20
+// STEP 21
+// STEP 22
